17e73fe2f6d157a53a5c27c6293bc75d21e957cb
ibradi3 <ibrahimasory22@hotmail.com>
2023-01-09 15:36:26 +0100

    bug-fix: self was not updating correctly

--- starter/src/main/bookselfChanger.js
+++ starter/src/main/bookselfChanger.js
@@ -12,6 +12,7 @@ function updateState(idBook, value, onShelfChange) {
 
 function Changer({ idBook, onShelfChange }) {
   const handleChange = (event) => {
+    console.log(event.target.value);
     updateState(idBook, event.target.value, onShelfChange);
   };
 
@@ -21,10 +22,10 @@ function Changer({ idBook, onShelfChange }) {
         <option value="none" disabled>
           Move to...
         </option>
-        <option value="currentlyReading">Currently Reading</option>
+        <option value="none">None</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
-        <option value="none">None</option>
+        <option value="currentlyReading">Currently Reading</option>
       </select>
     </div>
   );

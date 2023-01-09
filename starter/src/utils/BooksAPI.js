4757896c2e31e11153e18a54ddbaa35ee552b9cc
ibradi3 <ibrahimasory22@hotmail.com>
2023-01-09 15:43:42 +0100

    working perfectly

--- starter/src/utils/BooksAPI.js
+++ starter/src/utils/BooksAPI.js
@@ -4,8 +4,6 @@ let token = localStorage.token;
 
 if (!token) token = localStorage.token = Math.random().toString(36).substr(-8);
 
-console.log(token);
-
 const headers = {
   Accept: "application/json",
   Authorization: token,
@@ -14,7 +12,7 @@ const headers = {
 export const get = (bookId) =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then((res) => res.json())
-    .then((data) => data.book); 
+    .then((data) => data.book);
 
 export const getAll = () =>
   fetch(`${api}/books`, { headers })

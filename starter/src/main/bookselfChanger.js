0cb2323e3ec2665173b240fcae21069c440f5d7f
ibradi3 <ibrahimasory22@hotmail.com>
2023-01-10 23:02:25 +0100

    bug fix: dropdown

--- starter/src/main/bookselfChanger.js
+++ starter/src/main/bookselfChanger.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { get, update } from "./../utils/BooksAPI";
+import { useState } from "react";
 
 function updateState(idBook, value, onShelfChange) {
   get(idBook).then((book) => {
@@ -10,17 +11,23 @@ function updateState(idBook, value, onShelfChange) {
 }
 
 function Changer({ idBook, onShelfChange }) {
+  const [book, setBook] = useState({});
   const handleChange = (event) => {
     updateState(idBook, event.target.value, onShelfChange);
   };
 
+  useEffect(() => {
+    get(idBook).then((book) => {
+      setBook(book);
+    });
+  });
+
   return (
     <div className="book-shelf-changer">
-      <select onChange={handleChange}>
+      <select value={book.shelf} onChange={handleChange}>
         <option value="none" disabled>
           Move to...
         </option>
-        <option value="none"></option>
         <option value="none">None</option>
         <option value="wantToRead">Want to Read</option>
         <option value="read">Read</option>
